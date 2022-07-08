a9db37f9a3d491ffcd9002b063687afcbb3a9c5c
megamen932 <megamen932@gmail.com>
2022-07-08 18:54:51 +0300

    fix skips

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
             webhook_path=WEBHOOK_PATH,
             on_startup=startup,
             on_shutdown=save_exit,
-            skip_updates=True,
+            skip_updates=False,
             host=WEBAPP_HOST,
             port=WEBAPP_PORT)
         else:
