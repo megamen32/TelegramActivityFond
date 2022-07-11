e625af2a86e09c7304f9d77802ebc2ad7107cd44
Nickname <megamen932@gmail.com>
2022-07-11 07:43:47 +0300

    fix bugs 15

--- yappyUser.py
+++ yappyUser.py
@@ -102,7 +102,7 @@ class YappyUser:
         if self.reserved_amount>=0:
             t= f"Общий баланс: {self.coins:.1f}\n"
             t+=  f"Заморожено для исполнителей: {self.reserved_amount:.1f}\n"
-            t=  f"Доступный баланс: {(self.coins - self.reserved_amount):.1f}\n"
+            t=+  f"Доступный баланс: {(self.coins - self.reserved_amount):.1f}\n"
             return t
         else:
             return f"\nДоступный баланс: {self.coins - self.reserved_amount:.1f}\n"
